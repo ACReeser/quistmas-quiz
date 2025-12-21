d04c1ea5c7998b41815693e7e6edb885522c27f8
Alex Reeser <alexcreeser@gmail.com>
2025-12-21 12:43:23 -0600

    2025 questions 1

--- src/Game.tsx
+++ src/Game.tsx
@@ -28,43 +28,43 @@ function beginGameState(teams: string[]): IGameState{
   return {
     categories: [
       {
-        name: 'Mixolomoji',
+        name: 'Yummoji',
         rows: [
-          {points: 100, question: '\n🍎 cy+🚪', size: '22vh', answer: 'Apple Cider'}, 
-          {points: 200, question: '\n🌶️🍬 🐮+ca', size: '22vh', answer: 'Peppermint Mocha'},
-          {points: 300, question: '\n🌮🔥🍫', size: '22vh', answer: 'Mexican Hot Chocolate'},
-          {points: 400, question: '\n🔥🧈 🏃‍♂️-n+m', size: '21vh', answer: 'Hot Buttered Rum'}, 
-          {points: 500, question: '\n😾 and 🐭 (names)', size: '21vh', answer:'Tom and Jerry'},
+          {points: 100, question: '\n🦃🦆🐓', size: '22vh', answer: 'Turducken'}, 
+          {points: 200, question: '\n🎄🪵', size: '22vh', answer: 'Yule Log'},
+          {points: 300, question: '\n🫚🍞👨', size: '22vh', answer: 'Gingerbread Man'},
+          {points: 400, question: '\n🌶️🍬🐶', size: '21vh', answer: 'Peppermint Bark'}, 
+          {points: 500, question: '\n🍃🔑💩🛎️', size: '21vh', answer:'Figgy Pudding'},
         ]
       },
       {
-        name: 'Fa-la-la-la Fauna',
+        name: 'Holiday Horror',
         rows: [
-          {points: 100, question: "\nBright and starry reindeer?", size: '14vh', answer: 'Comet'}, 
-          {points: 200, question: "\nRobbie the reindeer's nemesis?", size: '14vh', answer: "Blitzen"}, 
-          {points: 300, question: "\nDecorates his doghouse with Christmas lights?", size: '14vh', answer: 'Snoopy'}, 
-          {points: 400, question: "\nHouse-invading turkey-eating hounds?", size: '16vh', answer: 'Bumpus\'s hound dogs'}, 
-          {points: 500, question: "Lost penguin from\n“Santa Claus is Comin' to Town”", size: '18vh', answer: 'Topper'}, 
+          {points: 100, question: "What fossil fuel does Santa famously leave for \"naughty\" children?", size: '14vh', answer: 'Coal'}, 
+          {points: 200, question: " In the song \"It’s the Most Wonderful Time of the Year,\" what 'scary' thing are people doing?", size: '14vh', answer: "Telling ghost stories"}, 
+          {points: 300, question: "What horned, Alpine \"Anti-Santa\" carries a sack to kidnap misbehaving kids", size: '14vh', answer: 'Krampus'}, 
+          {points: 400, question: "In 1908, which industry began officially designating candle-lit Christmas trees as an 'unacceptable risk'?", size: '16vh', answer: 'Fire insurance industry'}, 
+          {points: 500, question: "The predatory Jólakötturinn is a holiday monster from which Atlantic island nation?", size: '16vh', answer: 'Iceland'}, 
         ]
       },
       {
-        name: 'Christmas Casting',
+        name: 'Christmas Crime',
         rows: [
-          {points: 100, question: "Ebenezer Scrooge in\n“The Muppet Christmas Carol\" (1992)", size: '16vh', answer: 'Michael Caine'}, 
-          {points: 200, question: "Jovie in\n“Elf\"\n(2003)", size: '16vh', answer: "Zooey Deschanel"}, 
-          {points: 300, question: "Gus Polinkski in\n“Home Alone”\n(1990)", size: '16vh', answer: 'Johny Candy'}, 
-          {points: 400, question: "Overworked assistant Grace Cooley in “Scrooged”\n(1988)", size: '16vh', answer: 'Alfre Woodard'}, 
-          {points: 500, question: "Mrs. Parker in\n“A Christmas Story”\n(1983) ", size: '16vh', answer: 'Melinda Dillon'}, 
+          {points: 100, question: "In the movie Home Alone, the \"Wet Bandits\" earn their nickname by doing what to every house they burgle?", size: '16vh', answer: 'Leaving the water running'}, 
+          {points: 200, question: "A man dressed in a Santa suit robbed a San Francisco bank in Dec 2014, using what annual event to blend in and escape?", size: '14vh', answer: "SantaCon"}, 
+          {points: 300, question: "Until 1991, it was technically illegal to celebrate Christmas in which Caribbean island nation?", size: '16vh', answer: 'Cuba'}, 
+          {points: 400, question: "In 1826, 21 cadets at which military academy were punished for smuggling alcoholic eggnog into a barracks?", size: '14vh', answer: 'West Point'}, 
+          {points: 500, question: "In 1659, which North American colony passed a law making Christmas a criminal offense, punishable by a fine of five shillings?", size: '14vh', answer: 'Massachusetts'}, 
         ]
       },
       {
         name: 'Holiday Hodgepodge',
         rows: [
-          {points: 100, question: "How many ghosts show themselves to Scrooge in the original A Christmas Carol?", size: '14vh', answer: '4 (Marley, Past, Present, and Future)'}, 
-          {points: 200, question: "What was Frosty the Snowman’s nose made out of?", size: '16vh', answer: 'A button'}, 
-          {points: 300, question: "What hymn was written by a Pastor when a church organ broke?", size: '16vh', answer: 'Silent Night'}, 
-          {points: 400, question: "Which Nordic country donates the Christmas tree in London's Trafalgar Square?", size: '16vh', answer: 'Norway'}, 
-          {points: 500, question: "The bones of St. Nicholas of Myra were stolen in 1087 by sailors from this Mediterranean country?", size: '13vh', answer: 'Italy, from the city of Bari'}
+          {points: 100, question: "Famous animator Chuck Jones recolored which holiday villain to green for the 1966 TV special?", size: '14vh', answer: 'The Grinch (from b&w)'}, 
+          {points: 200, question: "Legend says a German choirmaster created what treat in 1670 to keep children quiet during long church services?", size: '14vh', answer: 'Candy Cane'}, 
+          {points: 300, question: "To deliver presents to every household on Earth in one night, physicists estimate Santa would have to travel at roughly 0.5% of what measure?", size: '14vh', answer: 'Speed of light'}, 
+          {points: 400, question: "According to the Guinness World Records, the tallest Christmas tree ever displayed was a 221 ft. Douglas Fir erected in 1950 in what rainy city?", size: '14vh', answer: 'Seattle'}, 
+          {points: 500, question: "What is the highest-grossing Christmas movie of all time (when adjusted for inflation), at a cool $1.2 billion?", size: '14vh', answer: 'Home Alone'}
         ]
       }
     ],
---
+++ src/Questions2024.tsx
@@ -0,0 +1,86 @@
+
+export const _2024_Questions_1 = [
+  {
+    name: 'Mixolomoji',
+    rows: [
+      {points: 100, question: '\n🔥🍫', size: '22vh', answer: 'Hot Chocolate'}, 
+      {points: 200, question: '\n🍊🍍🍎🪵🌿', size: '22vh', answer: 'Wassail'},
+      {points: 300, question: '\n🎄🥊', size: '21vh', answer: 'Christmas Punch'}, 
+      {points: 400, question: '\n🌵🥚🧠', size: '21vh', answer:'Spiked Eggnog'},
+      {points: 500, question: '\n🫏🍷', size: '22vh', answer: 'Mulled Wine'},
+    ]
+  },
+  {
+    name: 'Fa-la-la-la Fauna',
+    rows: [
+      {points: 100, question: "\nThe Grinch's\ndog / reindeer?", size: '14vh', answer: 'Max'}, 
+      {points: 200, question: "\nFormerly unemployed navigator reindeer?", size: '14vh', answer: "Rudolph"}, 
+      {points: 300, question: "\nThe Nutcracker's towering nemesis?", size: '16vh', answer: 'The Mouse / Rat King'}, 
+      {points: 400, question: "\nHalloween Town's bestest boy?", size: '16vh', answer: 'Zero'}, 
+      {points: 500, question: "____ the extended auditory appendaged ungulate?", size: '18vh', answer: 'Nestor'}, 
+    ]
+  },
+  {
+    name: 'Christmas Casting',
+    rows: [
+      {points: 100, question: "The Grinch in\n“How The Grinch Stole Christmas\"\n(2000)", size: '16vh', answer: 'Jim Carrey'}, 
+      {points: 200, question: "The Conductor in\n“The Polar Express\"\n(2004)", size: '16vh', answer: "Tom Hanks"}, 
+      {points: 300, question: "George Bailey in\n“It’s a Wonderful Life”\n(1946) ", size: '16vh', answer: 'Jimmy Stewart'}, 
+      {points: 400, question: "Kate McCallister in\n“Home Alone”\n(1990)", size: '16vh', answer: 'Catherine O\'Haram'}, 
+      {points: 500, question: "Shelter manager Karen Allen in “Scrooged”\n(1988)", size: '16vh', answer: 'Karen Allen'}, 
+    ]
+  },
+  {
+    name: 'Holiday Hodgepodge',
+    rows: [
+      {points: 100, question: "What “spy” hides around the house, telling Santa who has been naughty and nice? Elf on a Shelf?", size: '14vh', answer: 'Elf on a Shelf'}, 
+      {points: 200, question: "What color are the berries of mistletoe?", size: '16vh', answer: 'White'}, 
+      {points: 300, question: "Which Christmas song was the first song broadcast in space?", size: '16vh', answer: 'Jingle Bells'}, 
+      {points: 400, question: "Which arid U.S. state is the home to the town of Snowflake?", size: '16vh', answer: 'Arizona'}, 
+      {points: 500, question: "In what year did Edison Co. VP Edward Johnson decorate a tree with the first electric Christmas lights? (Closest wins)", size: '13vh', answer: '1882'}
+    ]
+  }
+];
+
+export const _2024_Questions_2 = [
+  {
+    name: 'Mixolomoji',
+    rows: [
+      { points: 100, question: '\n🍎 cy+🚪', size: '22vh', answer: 'Apple Cider' },
+      { points: 200, question: '\n🌶️🍬 🐮+ca', size: '22vh', answer: 'Peppermint Mocha' },
+      { points: 300, question: '\n🌮🔥🍫', size: '22vh', answer: 'Mexican Hot Chocolate' },
+      { points: 400, question: '\n🔥🧈 🏃‍♂️-n+m', size: '21vh', answer: 'Hot Buttered Rum' },
+      { points: 500, question: '\n😾 and 🐭 (names)', size: '21vh', answer: 'Tom and Jerry' },
+    ]
+  },
+  {
+    name: 'Fa-la-la-la Fauna',
+    rows: [
+      { points: 100, question: "\nBright and starry reindeer?", size: '14vh', answer: 'Comet' },
+      { points: 200, question: "\nRobbie the reindeer's nemesis?", size: '14vh', answer: "Blitzen" },
+      { points: 300, question: "\nDecorates his doghouse with Christmas lights?", size: '14vh', answer: 'Snoopy' },
+      { points: 400, question: "\nHouse-invading turkey-eating hounds?", size: '16vh', answer: 'Bumpus\'s hound dogs' },
+      { points: 500, question: "Lost penguin from\n“Santa Claus is Comin' to Town”", size: '18vh', answer: 'Topper' },
+    ]
+  },
+  {
+    name: 'Christmas Casting',
+    rows: [
+      { points: 100, question: "Ebenezer Scrooge in\n“The Muppet Christmas Carol\" (1992)", size: '16vh', answer: 'Michael Caine' },
+      { points: 200, question: "Jovie in\n“Elf\"\n(2003)", size: '16vh', answer: "Zooey Deschanel" },
+      { points: 300, question: "Gus Polinkski in\n“Home Alone”\n(1990)", size: '16vh', answer: 'Johny Candy' },
+      { points: 400, question: "Overworked assistant Grace Cooley in “Scrooged”\n(1988)", size: '16vh', answer: 'Alfre Woodard' },
+      { points: 500, question: "Mrs. Parker in\n“A Christmas Story”\n(1983) ", size: '16vh', answer: 'Melinda Dillon' },
+    ]
+  },
+  {
+    name: 'Holiday Hodgepodge',
+    rows: [
+      { points: 100, question: "How many ghosts show themselves to Scrooge in the original A Christmas Carol?", size: '14vh', answer: '4 (Marley, Past, Present, and Future)' },
+      { points: 200, question: "What was Frosty the Snowman’s nose made out of?", size: '16vh', answer: 'A button' },
+      { points: 300, question: "What hymn was written by a Pastor when a church organ broke?", size: '16vh', answer: 'Silent Night' },
+      { points: 400, question: "Which Nordic country donates the Christmas tree in London's Trafalgar Square?", size: '16vh', answer: 'Norway' },
+      { points: 500, question: "The bones of St. Nicholas of Myra were stolen in 1087 by sailors from this Mediterranean country?", size: '13vh', answer: 'Italy, from the city of Bari' }
+    ]
+  }
+];
